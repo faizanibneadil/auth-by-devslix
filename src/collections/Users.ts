861a3b17f2638dc5bccbf81527dde966ac19f0f1
Faizan Adil <faizanibneadil1@google.com>
2026-05-20 23:35:02 +0500

    auth service init

---
+++ src/collections/Users.ts
@@ -0,0 +1,41 @@
+import type { CollectionConfig } from 'payload'
+
+export const Users: CollectionConfig = {
+  slug: 'users',
+  admin: {
+    useAsTitle: 'email',
+  },
+  auth: {
+    loginWithUsername: {
+      allowEmailLogin: true,
+      requireUsername: true,
+      requireEmail: false
+    },
+    cookies: {
+      domain: '.devslix.com', // Sab se important line: Cookie ab saare subdomains par chalegi
+      sameSite: 'Lax',
+      secure: true, // Production me HTTPS hona zaroori hai
+    },
+  },
+  fields: [
+    // Email added by default
+    // Add more fields as needed
+  ],
+  endpoints: [{
+    path: '/verify-session',
+    method: 'get',
+    handler: async (req) => {
+
+      if (!req.user) {
+        return Response.json({ authenticated: false }, {
+          status: 401
+        })
+      }
+
+      return Response.json({
+        authenticated: true,
+        user: req.user
+      })
+    }
+  }]
+}
